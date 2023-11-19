5eb1fa08453d1a12312b8f86088ebddc4e771d94
planeguy <planeguy@delek.ca>
2023-11-19 15:43:59 -0800

    we can't actually write

--- index.js
+++ index.js
@@ -7,8 +7,6 @@ import jsonfile from "jsonfile";
 let settings = await jsonfile.readFile("settings.json");
 let santas = await jsonfile.readFile("santas.json");
 
-let dropped = await jsonfile.readFile("dropped.json",{flag:'a+'}).catch(err=>({}));
-
 const santaarray = santas.flatMap(s=>[s.email,s.altMail]).filter(m=>!!m);
 const santabcc = santaarray.join(",");
 
@@ -16,16 +14,10 @@ const pop3 = new Pop3Command(settings.pop3);
 const xport = nodemailer.createTransport(settings.smtp);
 
 const mails = await pop3.UIDL();
-console.log(mails);
 let mailstring
 for(let i=0;i<mails.length;i++){
     mailstring = await pop3.RETR(mails[i][0]);
     let parsed = (await simpleParser(mailstring));
-
-    console.log(parsed.from.value[0].address);
-    console.log(`${parsed.subject} from ${parsed.from.value[0].address}`);
-    console.log(`${santaarray.indexOf(parsed.from.value[0].address)}`);
-
     if (santaarray.indexOf(parsed.from.value[0].address)>-1){
         await xport.sendMail({
             from:`"${parsed.from.value[0].name||parsed.from.value[0].address} via Santa"<${settings.email}>`,
@@ -35,8 +27,7 @@ for(let i=0;i<mails.length;i++){
             html:parsed.html
         });
     } else {
-        if (dropped[parsed.from.value[0].address]) dropped[parsed.from.value[0].address]++;
-        else dropped[parsed.from.value[0].address]=1;
+        console.log(`$cDROP: email from ${parsed.from.value[0].address}`,"color:red");
     }
     await pop3.command("DELE",mails[i][0]);
 }
